c17abeb85f8b9a05b8c0c05aa1acbc63f5160018
Sarah Maas <maass@oregonstate.edu>
2018-01-06 03:52:11 +0000

    pages loading as desired, CSS and JS moves on the same page need help:

---
+++ public/angular_conversion/js/app.js
@@ -0,0 +1,43 @@
+
+// set up angular app
+var myApp = angular.module('HomeworkBetApp', ['ngRoute']);
+
+
+// add routes
+
+myApp.config(['$routeProvider', function($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'main.html',
+      controller: 'routeController'
+    })
+    .when('/about', {
+      templateUrl: 'partials/about.html',
+      controller: 'pageController'
+    })
+    .when('/register', {
+      templateUrl: 'partials/register.html',
+      controller: 'pageController'
+    })
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'pageController'
+    })
+    .when('/contact', {
+      templateUrl: 'partials/contact.html',
+      controller: 'pageController'
+    });
+}]);
+
+myApp.controller('pageController', ['$scope', '$log', function($scope, $log) {
+  $log.log($scope);
+}]);
+
+
+myApp.controller('routeController', ['$scope', function($scope) {
+  $scope.template = {
+    banner: 'partials/banner.html',
+    about: 'partials/about.html',
+    contact: 'partials/contact.html'
+  }  
+}]);
