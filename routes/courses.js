4d346dbd37f9a96b3cb029f632ad0fb4c2adced0
Nathan Perkins <nperkins487@gmail.com>
2018-01-06 11:27:33 -0800

    routes and template for creating new course

--- routes/courses.js
+++ routes/courses.js
@@ -2,6 +2,9 @@ const express = require('express'),
       router = express.Router(),
       settings = require('../app_settings');
 
+const UserModel = require('../models/UserModel');
+const CourseModel = require('../models/CourseModel');
+
 router.get('/', function (req, res) {
     res.render('courses/index', {
         pageTitle: 'Courses',
@@ -9,9 +12,29 @@ router.get('/', function (req, res) {
 });
 
 router.get('/new', function (req, res) {
-    res.render('courses/new', {
-        pageTitle: 'New Course',
+    UserModel.find({})
+    .then(users => {
+        res.render('courses/new', {
+            pageTitle: 'New Course',
+            users: users,
+        });
+    }).catch(err => {
+        console.log(err);
     });
-})
+});
+
+router.post('/', (req, res) => {
+    UserModel.findById(req.body.userId)
+    .then(user => {
+        const courseData = req.body.course;
+        courseData.user = user
+        return CourseModel.create(courseData);
+    }).then(course => {
+        res.redirect('/users/' + String(course.user._id));
+    }).catch(err => {
+        console.log(err);
+        res.redirect('/courses/new');
+    });
+});
 
 module.exports = router;
