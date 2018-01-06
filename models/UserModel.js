426dd178a4c679ebc22a0960d420537003be8252
Nathan Perkins <nperkins487@gmail.com>
2018-01-05 20:49:57 -0800

    add UserModel method courses() for getting the user's courses

--- models/UserModel.js
+++ models/UserModel.js
@@ -1,16 +1,19 @@
 const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
+const CourseModel = require('./CourseModel');
+
 const userSchema = mongoose.Schema({
     firstName: {type: String, required: true},
     lastName: {type: String, required: true},
     email: {type: String, required: true },
     password: {type: String, required: true},
-    // courses: [
-    //     { type: Schema.Types.ObjectId, ref: 'Course' }
-    // ],
     created: {type: Date, default: Date.now, required: true},
 });
+
+userSchema.methods.courses = function() {
+    return CourseModel.find({ 'user': this });
+}
 
 const UserModel = mongoose.model('User', userSchema);
 
--- tests/UserModel.js
+++ tests/UserModel.js
@@ -7,11 +7,13 @@ const settings = require('../app_settings.json');
 
 const UserModel = require('../models/UserModel');
 const UserFactory = require('../factories/User');
+const CourseModel = require('../models/CourseModel');
+const CourseFactory = require('../factories/Course');
 
 const dbName = settings.db_name_test || "homework-bet-test";
 const dbUrl = settings.db_host + dbName;
 
-describe('User Tests', function() {
+describe('User Model Tests', function() {
   
     before(function (done) {
         mongoose.connect(dbUrl);
@@ -47,6 +49,30 @@ describe('User Tests', function() {
             assert();
         }).catch(function(err) {
             done();
+        });
+    }),
+
+    it("Method for getting the user's courses", function () {
+        const userData = UserFactory.random();
+        const courseData = CourseFactory.random();
+        let user = {};
+        let course = {};
+
+        return UserModel.create(userData).then(function (newUser) {
+            courseData.user = newUser;
+            user = newUser;
+            return CourseModel.create(courseData);
+        }).then(function (newCourse) {
+            course = newCourse;
+            return user.courses();
+        }).then(function (newCourse) {
+            return CourseModel.create(CourseFactory.random(user));
+        }).then(function(newCourse) {
+            return user.courses();
+        }).then(function (courses) {
+            assert.equal(courses.length, 2);
+            assert.equal(courses[0].name, course.name);
+            assert.equal(String(courses[0]._id), String(course._id));
         });
     }),
 
