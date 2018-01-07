17bd48f34bf693c00bcaf71debdb820371e15d33
Colin Powell <colingrahampowell@gmail.com>
2018-01-07 15:51:01 -0500

    added pool factory, added unit test to confirm functionality

--- factories/Pool.js
+++ factories/Pool.js
@@ -6,5 +6,13 @@ module.exports = {
             startDate: moment(),
             endDate: moment().add(3, 'months'),
         }
+    },
+    generateYear: (year) => {
+        return [
+            {startDate: new Date(year, 0, 1), endDate: new Date(year, 2, 31)},
+            {startDate: new Date(year,3, 1), endDate: new Date(year, 5, 30)},
+            {startDate: new Date(year, 6, 1), endDate: new Date(year, 8, 30)},
+            {startDate: new Date(year, 9, 1), endDate: new Date(year, 11, 31)},
+        ];
     }
 }
--- tests/PoolModel.js
+++ tests/PoolModel.js
@@ -62,6 +62,23 @@ describe('Pool Model Tests', function () {
         });
     });
 
+    it('should generate a list of pool start/end dates for 2018', () => {
+        pooldates = PoolFactory.generateYear(2018);
+
+        assert(pooldates[0].startDate.getTime() === (new Date(2018, 0, 1)).getTime() );
+        assert(pooldates[0].endDate.getTime() === (new Date(2018, 2, 31)).getTime() );
+
+        assert(pooldates[1].startDate.getTime() === (new Date(2018, 3, 1)).getTime() );
+        assert(pooldates[1].endDate.getTime() === (new Date(2018, 5, 30)).getTime() );
+
+        assert(pooldates[2].startDate.getTime() === (new Date(2018, 6, 1)).getTime() );
+        assert(pooldates[2].endDate.getTime() === (new Date(2018, 8, 30)).getTime() );
+
+        assert(pooldates[3].startDate.getTime() === (new Date(2018, 9, 1)).getTime() );
+        assert(pooldates[3].endDate.getTime() === (new Date(2018, 11, 31)).getTime() );
+
+    });
+
     after(function (done) {
         mongoose.connection.db.dropDatabase(function () {
             mongoose.connection.close(done);
