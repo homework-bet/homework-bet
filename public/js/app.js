02bf69cd7e37b10c65f7caccc3e843b78968d10f
Sarah Maas <maass@oregonstate.edu>
2018-01-07 05:14:24 +0000

    registration working, needs email validation added

--- public/js/app.js
+++ public/js/app.js
@@ -65,48 +65,55 @@ myApp.controller('campaignController', ['$scope', '$log', function($scope, $log)
 }]);
 
 // loginController
-myApp.controller('loginController', ['$scope', '$http', '$log', function($scope, $http, $log) {
-  $log.log($scope);
-  $log.log('loginController is not yet fully set up.');
-  $scope.data = {};
-  $scope.email = null;
-  $scope.password = null;
-
-  var request = $http.get('/api/login');    
-  request.success(function(data) {
-      $scope.loginFormData = data;
-      console.log(data);
-      console.log("SUCCESSFULLY GOT THE LOGIN PAGE STUFF!");
-  });
-  request.error(function(data){
-      console.log('Error: ' + data);
-  });
-
-
-  // needs http post request, rewriting /login express route as /api/login
-  $scope.loginUser = function (email, password) {
-    // set up data to post
-    var data = {
-      email: email,
-      password: password
-    };
-
-    // set up request
-    var request = $http.post('/api/login');
-    $http.post('/api/login', JSON.stringify(data)).then(function (response) {
-      if (response.data) {
-        $scope.msg = "Post Data Submitted Successfully!";
-        console.log($scope.msg);
-        $scope.loginResponse = response.data;
-        console.log($scope.loginResponse);
-      }
-    }, function (response) {
-      $scope.msg = "Service not Exists";
-      $scope.statusval = response.status;
-      $scope.statustext = response.statusText;
-      $scope.headers = response.headers();
+myApp.controller('loginController', ['$scope', '$http', '$location', '$log', 
+  function($scope, $http, $location, $log) {
+    $log.log($scope);
+    $log.log('loginController is not yet fully set up.');
+    $scope.data = {};
+    $scope.user = {};
+  
+    var request = $http.get('/api/login');    
+    request.success(function(data) {
+        $scope.loginFormData = data;
+        console.log(data);
+        console.log("SUCCESSFULLY GOT THE LOGIN PAGE STUFF!");
     });
-  };
+    request.error(function(data){
+        console.log('Error: ' + data);
+    });
+  
+  
+    // needs http post request, rewriting /login express route as /api/login
+    $scope.loginUser = function () {
+      console.log($scope.user);
+      // set up data to post
+      var data = {
+        email:    $scope.user.email,
+        password: $scope.user.password
+      };
+  
+      // set up request
+      $http.post('/api/login', JSON.stringify(data)).then(function (response) {
+        if (response.data) {
+          $scope.msg = "Post Data Submitted Successfully!";
+          console.log($scope.msg);
+          $scope.loginResponse = response.data;
+          console.log($scope.loginResponse);
+          // from here, we need to validate the response to determine where to go
+          if($scope.loginResponse.error) {
+            // TODO fix - this error passing is not correct syntax
+            $location.path('/404', {error: $scope.loginResponse.error});
+          } else {
+            $location.path('/profile');
+          }
+        }
+      }, function (response) {
+        $scope.msg = "Service not Exists";
+        $scope.statusval = response.status;
+        $scope.statustext = response.statusText;
+        $scope.headers = response.headers();
+      });
+    };
 }]);
 
 // profileController
@@ -115,42 +122,40 @@ myApp.controller('profileController', ['$scope', '$log', function($scope, $log) 
   $log.log('profileController is not yet set up.');
 }]);
 
-myApp.controller('registerController', ['$scope', '$rootScope', '$location', '$window', '$auth', 
-  function($scope, $rootScope, $location, $window, $auth) {
+myApp.controller('registerController', ['$scope', '$http', '$location', 
+  function($scope, $http, $location) {
     $scope.registerUser = function() {
-      $auth.signup($scope.user)
-        .then(function(response) {
-          $auth.setToken(response);
-          $rootScope.currentUser = response.data.user;
-          $window.localStorage.user = JSON.stringify(response.data.user);
-          $location.path('/');
-        })
-        .catch(function(response) {
-          $scope.messages = {
-            error: Array.isArray(response.data) ? response.data : [response.data]
-          };
-        });
+      console.log($scope.user);
+        var data = {
+          firstName:  $scope.user.firstName,
+          lastName:   $scope.user.lastName,
+          password:   $scope.user.password, 
+          email:      $scope.user.email
+      };
+      // set up request
+      $http.post('/api/register', JSON.stringify(data)).then(function (response) {
+        if (response.data) {
+          $scope.msg = "Post Register Data Submitted Successfully!";
+          console.log($scope.msg);
+          $scope.registerResponse = response.data;
+          console.log($scope.registerResponse);
+          // from here, we need to validate the response to determine where to go
+          if($scope.registerResponse.error) {
+            // TODO fix - this error passing is not correct syntax
+            $location.path('/404', {error: $scope.registerResponse.error});
+          } else {
+            $location.path('/profile');
+          }
+        }
+      }, function (response) {
+        $scope.msg = "Service not Exists";
+        $scope.statusval = response.status;
+        $scope.statustext = response.statusText;
+        $scope.headers = response.headers();
+      });
     };
 
-    $scope.authenticate = function(provider) {
-      $auth.authenticate(provider)
-        .then(function(response) {
-          $rootScope.currentUser = response.data.user;
-          $window.localStorage.user = JSON.stringify(response.data.user);
-          $location.path('/');
-        })
-        .catch(function(response) {
-          if (response.error) {
-            $scope.messages = {
-              error: [{ msg: response.error }]
-            };
-          } else if (response.data) {
-            $scope.messages = {
-              error: [response.data]
-            };
-          }
-        });
-    };
+
 }]);
 
 
