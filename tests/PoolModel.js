6e9b61938fd78609de0f7261f72f2359992fa559
Nathan Perkins <nperkins487@gmail.com>
2018-01-06 18:03:47 -0800

    add first test for saving a valid pool

---
+++ tests/PoolModel.js
@@ -0,0 +1,64 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const chai = require('chai');
+const assert = chai.assert;
+const expect = chai.expect;
+const settings = require('../app_settings.json');
+
+const UserModel = require('../models/UserModel');
+const UserFactory = require('../factories/User');
+const CourseModel = require('../models/CourseModel');
+const CourseFactory = require('../factories/Course');
+const PoolModel = require('../models/PoolModel');
+const PoolFactory = require('../factories/Pool');
+
+const dbName = settings.db_name_test || "homework-bet-test";
+const dbUrl = settings.db_host + dbName;
+
+describe('Pool Model Tests', function () {
+
+    before(function (done) {
+        mongoose.connect(dbUrl);
+        const db = mongoose.connection;
+        db.on('error', console.error.bind(console, 'connection error'));
+        db.once('open', function () {
+            console.log('mongoose: connected to ' + dbUrl);
+            done();
+        });
+    }),
+
+    it('should save a valid pool to the database.', function (done) {
+        const userData = UserFactory.random();
+        const courseData = CourseFactory.random({});
+
+        UserModel.create(userData)
+            .then(user => {
+                courseData.user = user;
+                return CourseModel.create(courseData);
+            }).then(course => {
+                const poolData = {
+                    courses: [course],
+                    startDate: course.startDate,
+                    endDate: course.endDate,
+                };
+
+                return PoolModel.create(poolData);
+            }).then(pool => {
+                course = pool.courses[0];
+                assert.equal(course.name, courseData.name);
+                assert.equal(course.startDate, courseData.startDate);
+                assert.equal(course.endDate, courseData.endDate);
+                done();
+            }).catch(err => {
+                console.log("Unable to save valid pool.");
+                assert();
+            }
+        );
+    });
+
+    after(function (done) {
+        mongoose.connection.db.dropDatabase(function () {
+            mongoose.connection.close(done);
+        });
+    })
+});
