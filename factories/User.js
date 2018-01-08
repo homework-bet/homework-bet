2fb08f245ea448d39ee9f00f261747c9d2070b5e
Nathan Perkins <nperkins487@gmail.com>
2018-01-07 17:40:18 -0800

    add createRandom() to UserFactory

--- factories/User.js
+++ factories/User.js
@@ -3,33 +3,37 @@
  */
 
 const faker = require('faker');
+const UserModel = require('../models/UserModel')
+
+// returns an object with specified properties suitable 
+// for a User entry in the database.
+const specifed = (fName, lName, pass, email) => {
+    return {
+        firstName: fName,
+        lastName: lName,
+        password: pass,
+        email: email
+    };
+}
+
+// returns an object with randomly-generated properties suitable 
+// for a User entry in the database.
+const random = () => {
+    return {
+        firstName: faker.name.firstName(),
+        lastName: faker.name.lastName(),
+        password: faker.internet.password(),
+        email: faker.internet.email(),
+    };
+}
+
+const createRandom = () => {
+    return UserModel.create(random());
+}
 
 module.exports = {
-
-    // returns an object with specified properties suitable 
-    // for a User entry in the database.
-
-    specified: function(fName, lName, pass, email) {
-        return {
-            firstName: fName, 
-            lastName: lName, 
-            password: pass,
-            email: email
-
-        };
-    },
-    
-    // returns an object with randomly-generated properties suitable 
-    // for a User entry in the database.
-
-    random: function() {
-        return { 
-            firstName: faker.name.firstName(),
-            lastName: faker.name.lastName(),
-            password: faker.internet.password(),
-            email: faker.internet.email(),
-        };
-
-    }
+    specifed: specifed,
+    random: random,
+    createRandom: createRandom(),
 };
 
